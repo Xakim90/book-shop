c7e7d7219a74b56ac6e40331aee111e0c4a494eb
Sardor1498 <tsardor1498@gmail.com>
2021-08-08 20:53:11 +0500

    eski

---
+++ src/pages/Register.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Axios from 'axios';
+
+const Register = () => {
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const url = "https://deliver-project.herokuapp.com/api/register"
+
+    const submit = async (e) => {
+        e.preventDefault()
+        Axios.post(url, user);
+    }
+
+    const setUserData = (event) => {
+        setUser({
+            ...user,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    return (
+        <>
+            <div className="flex flex-wrap content-center h-screen justify-center">
+                <form onSubmit={submit} autoComplete="off">
+                    <h1 className="text-center font-bold text-lg">Registration</h1>
+                    <br />
+                    <TextField
+                        id="outlined-basic"
+                        label="Name"
+                        variant="outlined"
+                        value={user.name}
+                        name="name"
+                        onChange={setUserData}
+                    />
+                    <br />
+                    <br />
+                    <TextField
+                        id="outlined-basic"
+                        label="Email"
+                        variant="outlined"
+                        value={user.email}
+                        name="email"
+                        onChange={setUserData}
+                    />
+                    <br />
+                    <br />
+                    <TextField
+                        id="outlined-basic"
+                        label="Password"
+                        variant="outlined"
+                        value={user.password}
+                        name="password"
+                        onChange={setUserData}
+                    />
+                    <br />
+                    <br />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                    >
+                        Register
+                    </Button>
+                    <Button
+                        type="reset"
+                        style={{ marginLeft: "25px" }}
+                        variant="contained"
+                        color="secondary"
+                    >
+                        Cancel
+                    </Button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Register;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,43 +1,10 @@
-import Blade from "../pages/Blade";
-import Warface from "../pages/Warface/Warface";
-import Armata from "../pages/Armata";
 import AdbIcon from '@material-ui/icons/Adb';
 import Test from "../pages/Test";
 import Sardor from "../pages/Sardor";
 import Sirojiddin from "../pages/Sirojiddin";
+import Register from "../pages/Register";
 
 export const ROUTES = [
-    
-    {
-        name: "Conqueror's Blade",
-        icon: <AdbIcon />,
-        url: "/blade",
-        component: () => <Blade />
-    },
-    {
-        name: "Warface",
-        icon: <AdbIcon />,
-        url: "/warface",
-        component: () => <Warface />
-    },
-    {
-        name: "Armored Warfare: Проект Армата",
-        icon: <AdbIcon />,
-        url: "/armata",
-        component: () => <Armata />
-    },
-    {
-        name: "CrossFire",
-        icon: <AdbIcon />,
-        url: "/crossfire",
-        component: () => <Blade />
-    },
-    {
-        name: "Skyforge",
-        icon: <AdbIcon />,
-        url: "/skyforge",
-        component: () => <Blade />
-    },
     {
         name: "Test",
         icon: <AdbIcon />,
@@ -55,5 +22,11 @@ export const ROUTES = [
         icon: <AdbIcon />,
         url: "/sirojiddin",
         component: () => <Sirojiddin />
+    },
+    {
+        name: "Register",
+        icon: <AdbIcon />,
+        url: "/register",
+        component: () => <Register />
     }
 ]
