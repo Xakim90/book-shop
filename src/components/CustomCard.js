900ff085d34cede549989ccf486ecbd3ce7a7314
Sardor1498 <tsardor1498@gmail.com>
2021-09-19 16:58:45 +0500

    sss

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-<<<<<<< HEAD
 const CustomCard = ({ img, img2, title, price, id, doublePhoto = true }) => {
-=======
-const CustomCard = ({ img, img2, title, price, doublePhoto = true }) => {
->>>>>>> a2f67760b797c9531bd0dee851fcf58d638ccc1a
     const [photo, setPhoto] = useState(false);
 
     return (
         <div>
-<<<<<<< HEAD
             {/* <img
                 className="w-full h-96 img-fluid cursor-pointer"
                 onMouseOver={() => setPhoto(doublePhoto)}
@@ -18,8 +13,6 @@ const CustomCard = ({ img, img2, title, price, doublePhoto = true }) => {
                 src={!photo ? img : img2}
                 alt="Book"
             /> */}
-=======
->>>>>>> a2f67760b797c9531bd0dee851fcf58d638ccc1a
             {
                 doublePhoto ? (
                     <img
@@ -30,18 +23,11 @@ const CustomCard = ({ img, img2, title, price, doublePhoto = true }) => {
                         alt="Book"
                     />
                 ) : (
-<<<<<<< HEAD
                     <Link to={"/product/" + img.replace("https://", "")}>
                         <img className="cursor-pointer" src={img} alt="books" />
                     </Link>
                 )
             }
-=======
-                    <img src={img} alt="" />
-                )
-            }
-
->>>>>>> a2f67760b797c9531bd0dee851fcf58d638ccc1a
             <h1 className="text-xl">{title}</h1>
             <h1>{price}</h1>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import React from 'react'
 import CustomCard from '../components/CustomCard';
 import { images } from '../routes/images';
 import { Link } from 'react-router-dom';
 
-=======
-import React from 'react';
-import CustomCard from '../components/CustomCard';
->>>>>>> a2f67760b797c9531bd0dee851fcf58d638ccc1a
 
 const images = [
     {
@@ -39,27 +34,19 @@ const Home = () => {
     return (
         <div>
             <div>
-<<<<<<< HEAD
                 <img
                     className="w-full"
-=======
-                <img className="w-full"
->>>>>>> a2f67760b797c9531bd0dee851fcf58d638ccc1a
                     src="https://cdn.shopify.com/s/files/1/0880/2454/files/0_bda999bf-aeb8-4cec-8026-616365c35b8b_1500x.jpg?v=1622668580"
                     alt=""
                 />
             </div>
-<<<<<<< HEAD
             <br />
             <hr />
             <br />
-=======
->>>>>>> a2f67760b797c9531bd0dee851fcf58d638ccc1a
             <div className="grid grid-cols-4 gap-4">
                 {
                     images.map((item, index) => {
                         return (
-<<<<<<< HEAD
                             <Link key={item+index} to="/cart">
                                 <CustomCard
                                     key={index}
@@ -70,16 +57,6 @@ const Home = () => {
                                     doublePhoto={false}
                                 />
                             </Link>
-=======
-                            <CustomCard
-                                key={index}
-                                img={item.photo}
-                                img2={item.photo2}
-                                title={item.name}
-                                price={item.price}
-                                doublePhoto={false}
-                            />
->>>>>>> a2f67760b797c9531bd0dee851fcf58d638ccc1a
                         )
                     })
                 }
